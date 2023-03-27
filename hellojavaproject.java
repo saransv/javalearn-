f6f66d4d5ce1a3027ef50c9fd6347294be739821
saransv <54670329+saransv@users.noreply.github.com>
2023-03-27 09:21:23 +0530

    Add files via upload

---
+++ hellojavaproject.java
@@ -0,0 +1,10 @@
+package hellojava;
+
+public class hellojavaproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Frist java program!");
+	}
+
+}
